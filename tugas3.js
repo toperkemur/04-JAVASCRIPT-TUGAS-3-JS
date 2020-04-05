ef37048b52a40a322c1beb1c1ca304e6339e7389
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-04-05 14:20:46 +0700

    Initial Commit
    Menambahkan tugas3.js

---
+++ tugas3.js
@@ -0,0 +1,20 @@
+function kembali(x){
+    console.log(x);
+    return x * 10;
+}
+
+//Jalan 2
+// const kembali2 = function(x){
+//   console.log(x);
+//   return x * 20;
+// }
+//
+// const kembali3 = (x) => {
+//   console.log(x);
+//   return x*30;
+// }
+
+//jalan 3
+console.log(kembali(5));
+// console.log(kembali2(3));
+// console.log(kembali3(7));
